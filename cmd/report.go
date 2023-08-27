23f1d3e911397999150e16227167f3dc8c0963d6
Tariq <chill.tariq@gmail.com>
2023-08-27 18:28:36 -0400

    build initial reports poc

--- cmd/report.go
+++ cmd/report.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"text/template"
+	"time"
 
+	db "github.com/redgoose/daikin-one/internal"
 	"github.com/spf13/cobra"
 )
 
@@ -19,7 +23,39 @@ var reportSummaryCmd = &cobra.Command{
 	Args:  cobra.NoArgs,
 	Short: "Generates summary report",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(dbPath, deviceId)
+		var data = db.GetDataForDay(dbPath, deviceId, time.Now())
+
+		type Foo struct {
+			Title           string
+			Hour            []string
+			TempIndoor      []float32
+			TempOutdoor     []float32
+			HumidityIndoor  []float32
+			HumidityOutdoor []float32
+			CoolSetpoint    []float32
+			HeatSetpoint    []float32
+			RunTime         []int
+		}
+
+		data2 := Foo{
+			Title:           time.Now().Format("2006-01-02"),
+			Hour:            data.Hour,
+			TempIndoor:      data.TempIndoor,
+			TempOutdoor:     data.TempOutdoor,
+			HumidityIndoor:  data.HumidityIndoor,
+			HumidityOutdoor: data.HumidityOutdoor,
+			CoolSetpoint:    data.CoolSetpoint,
+			HeatSetpoint:    data.HeatSetpoint,
+			RunTime:         data.RunTime,
+		}
+
+		tmpl := template.Must(template.ParseFiles("templates/chart.tmpl"))
+
+		var doc bytes.Buffer
+		tmpl.Execute(&doc, data2)
+		s := doc.String()
+		fmt.Println(s)
+
 	},
 }
 
--- internal/db.go
+++ internal/db.go
@@ -7,6 +7,14 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	EquipmentStatusCool     = 1
+	EquipmentStatusOvercool = 2
+	EquipmentStatusHeat     = 3
+	EquipmentStatusFan      = 4
+	EquipmentStatusIdle     = 5
+)
+
 type DeviceData struct {
 	DeviceId        string
 	TempIndoor      float32
@@ -16,6 +24,17 @@ type DeviceData struct {
 	CoolSetpoint    float32
 	HeatSetpoint    float32
 	EquipmentStatus int
+}
+
+type DayData struct {
+	Hour            []string
+	TempIndoor      []float32
+	TempOutdoor     []float32
+	HumidityIndoor  []float32
+	HumidityOutdoor []float32
+	CoolSetpoint    []float32
+	HeatSetpoint    []float32
+	RunTime         []int
 }
 
 func LogData(dbPath string, data DeviceData) {
@@ -39,6 +58,75 @@ func LogData(dbPath string, data DeviceData) {
 		data.EquipmentStatus,
 	)
 	checkErr(err)
+}
+
+func GetDataForDay(dbPath string, deviceId string, day time.Time) DayData {
+	db, err := sql.Open("sqlite3", dbPath)
+	checkErr(err)
+
+	rows, err := db.Query(`
+		select
+			substr(timestamp, 12, 2) || ":00" as hour,
+			round(avg(temp_indoor), 2) as temp_indoor,
+			round(avg(temp_outdoor), 2) as temp_outdoor,
+			round(avg(humidity_indoor), 2) as humidity_indoor,
+			round(avg(humidity_outdoor), 2) as humidity_outdoor,
+			round(avg(cool_setpoint), 2) as cool_setpoint,
+			round(avg(heat_setpoint), 2) as heat_setpoint,
+			sum(
+				case when equipment_status = ? or equipment_status = ? or equipment_status = ?
+				then 5
+				else 0
+				end
+			) as run_time
+		from daikin
+		where substr(timestamp, 0, 11) = ?
+		and device_id = ?
+		group by substr(timestamp, 0, 14);
+	`, EquipmentStatusCool, EquipmentStatusOvercool, EquipmentStatusHeat, day.Format("2006-01-02"), deviceId)
+	checkErr(err)
+
+	defer rows.Close()
+
+	var dayData DayData
+
+	for rows.Next() {
+		var (
+			hour            string
+			tempIndoor      float32
+			tempOutdoor     float32
+			humidityIndoor  float32
+			humidityOutdoor float32
+			coolSetpoint    float32
+			heatSetpoint    float32
+			runTime         int
+		)
+		err := rows.Scan(
+			&hour,
+			&tempIndoor,
+			&tempOutdoor,
+			&humidityIndoor,
+			&humidityOutdoor,
+			&coolSetpoint,
+			&heatSetpoint,
+			&runTime,
+		)
+		checkErr(err)
+
+		dayData.Hour = append(dayData.Hour, hour)
+		dayData.TempIndoor = append(dayData.TempIndoor, tempIndoor)
+		dayData.TempOutdoor = append(dayData.TempOutdoor, tempOutdoor)
+		dayData.HumidityIndoor = append(dayData.HumidityIndoor, humidityIndoor)
+		dayData.HumidityOutdoor = append(dayData.HumidityOutdoor, humidityOutdoor)
+		dayData.CoolSetpoint = append(dayData.CoolSetpoint, coolSetpoint)
+		dayData.HeatSetpoint = append(dayData.HeatSetpoint, heatSetpoint)
+		dayData.RunTime = append(dayData.RunTime, runTime)
+	}
+
+	err = rows.Err()
+	checkErr(err)
+
+	return dayData
 }
 
 func checkErr(err error) {
